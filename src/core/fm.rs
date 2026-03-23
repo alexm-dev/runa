0d942cf1789ddc5065a35c4b1604fb26fc4b2bfd
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-03-23 22:28:35 +0100

    fix: change users function calls to uzers

--- src/core/fm.rs
+++ src/core/fm.rs
@@ -196,9 +196,9 @@ impl FileInfo {
         #[cfg(unix)]
         {
             use std::os::unix::fs::MetadataExt;
-            use users::{get_group_by_gid, get_user_by_uid};
+            use uzers::{get_group_by_gid, get_user_by_uid};
 
-            if let Ok(meta) = symlink_metadata(_path) {
+            if let Ok(meta) = symlink_metadata(path) {
                 let uid = meta.uid();
                 let gid = meta.gid();
 
